edd4859607aa43b89685fbf1d9f6314f093b4b44
duygu <1803021028@student.beykent.edu.tr>
2022-07-28 16:44:10 +0300

    feat: added popup and ng-boostrap

--- Web/src/app/app.module.ts
+++ Web/src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
