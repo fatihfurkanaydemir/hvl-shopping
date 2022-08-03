491f3455d1e67757c35e2b14e13477c0543998d5
yhasansenyurt <yhsenyurt@gmail.com>
2022-08-03 08:43:41 +0300

    login and register page upload.

---
+++ Web/src/app/login-page/login-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbRatingConfig } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css']
+})
+export class LoginPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
