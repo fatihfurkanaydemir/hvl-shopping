dbb02b8fecbaf13f58884fab211a2195f3524d5f
Voul <18155057@mersin.edu.tr>
2022-08-06 23:55:32 +0300

    Form operations are working with reactive forms.

--- Web/src/app/user-profile/user-profile.component.ts
+++ Web/src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormGroupName, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-profile',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
+  userDisplayForm: FormGroup;
 
-  constructor() { }
+  constructor(fb: FormBuilder) { 
+    this.userDisplayForm = new FormGroup({
+      isim: new FormControl('', Validators.required),
+      soyIsim: new FormControl('', Validators.required),
+      sifre: new FormControl('', Validators.required),
+      telNo: new FormControl('', Validators.required)
+    });
+  }
 
   ngOnInit(): void {
+    this.createUserDisplayForm();
+  }
+
+  createUserDisplayForm(){
+    this.userDisplayForm = new FormGroup({
+      isim: new FormControl('', Validators.required),
+      soyIsim: new FormControl('', Validators.required),
+      sifre: new FormControl('', Validators.required),
+      telNo: new FormControl('', Validators.required)
+    })
+  }
+  
+  onUserDisplayFormSubmit(){
+    console.log(this.userDisplayForm.value);
   }
 
 }
