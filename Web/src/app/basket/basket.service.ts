cac296a078a8a7a47504464870afdb925d8e8eb3
Murat Şahin <18155057@mersin.edu.tr>
2022-08-10 13:28:55 +0300

    Added some methods into the basketService.

--- Web/src/app/basket/basket.service.ts
+++ Web/src/app/basket/basket.service.ts
@@ -2,7 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, map } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { IBasket } from '../models/IBasket';
+import { IBasket, IBasketItem } from '../models/IBasket';
+import { IProduct } from '../models/IProduct';
 
 @Injectable({
   providedIn: 'root'
@@ -35,4 +36,21 @@ export class BasketService {
   getCurrentBasketValue(){
     return this.basketSource.value;
   }
+
+  addItemToBasket(item: IProduct, quantity=1){
+    const itemToAdd: IBasketItem = this.mapProductItemToBasketItem(item, quantity);
+  }
+
+  mapProductItemToBasketItem(item: IProduct, quantity: number): IBasketItem {
+    return {
+      id: item.id,
+      productName: item.name,
+      price: item.price,
+      pictureUrl: item.images[0].url,
+      quantity,
+      
+    }
+  }
 }
+
+
