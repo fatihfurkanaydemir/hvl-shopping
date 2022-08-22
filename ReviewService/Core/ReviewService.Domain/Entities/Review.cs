ef4d7240cbd4f16b521572c60208d3b6f16e84be
duygu <1803021028@student.beykent.edu.tr>
2022-08-22 11:14:09 +0300

    mongo atlas connectionstring and project commands added

--- ReviewService/Core/ReviewService.Domain/Entities/Review.cs
+++ ReviewService/Core/ReviewService.Domain/Entities/Review.cs
@@ -14,12 +14,15 @@ namespace ReviewService.Domain.Entities
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; } = ObjectId.GenerateNewId().ToString();
+        [BsonElement("ProductId")]
         public int ProductId { get; set; }
-
         [BsonElement("Name")]
         public string Name { get; set; }
+        [BsonElement("LastName")]
         public string LastName { get; set; }
+        [BsonElement("Comment")]
         public string Comment { get; set; }
+        [BsonElement("Rate")]
         public int Rate { get; set; }
     }
 }
